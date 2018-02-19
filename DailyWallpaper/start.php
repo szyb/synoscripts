0da8c53ebc7a8d208d0b0d88610216b635253d99
Szymon Belus <sbelus@gmail.com>
2018-02-19 22:37:10 +0100

    Setup wallpaper provider as a script parameter
    Fixes #4

--- DailyWallpaper/start.php
+++ DailyWallpaper/start.php
@@ -6,17 +6,31 @@ require_once 'UnsplashProvider.php';
 use \synoscripts\BingProvider;
 use \synoscripts\UnsplashProvider;
 
-$providerName = "Bing";
-//$providerName = "Unsplash";
+$providerName = "Bing"; //default
 
 
 if (isset($argv[1])==false)
 {
-    echo "usage: $argv[0] 'username' 'installationDir'\n";
+    echo "usage: $argv[0] 'username' 'installationDir' [Bing|Unsplash|Random]\n";
     exit;
 }
 $userName=$argv[1];
 $installationDir=$argv[2];
+
+if (isset($argv[3]))
+{
+    if ($argv[3] == "Unsplash")
+        $providerName = "Unsplash";
+    else if ($argv[3] == "Random")
+    {
+        $r = rand(1,2);
+        echo $r;
+        if ($r == 1) $providerName = "Bing";
+        else if ($r == 2) $providerName = "Unsplash";
+    }
+}
+
+echo "Choosen provider: ".$providerName."\n";
 
 if ($providerName == "Bing")
     $provider = new BingProvider($userName, $installationDir);
